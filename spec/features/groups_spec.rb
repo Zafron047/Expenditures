e6d22c5e17ed0430281657ecb4dc3873be129e0a
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-10-18 19:14:57 +0600

    Addd capybara testings

--- spec/features/groups_spec.rb
+++ spec/features/groups_spec.rb
@@ -4,21 +4,43 @@ RSpec.describe 'User authentication', type: :feature do
   before(:each) do
     DatabaseCleaner.clean
   end
-  it 'allows a user to register' do
-    visit new_user_registration_path
-    fill_in 'name', with: 'New User'
-    fill_in 'Email', with: 'newuser@example.com'
+
+  before(:each) do
+    user = FactoryBot.create(:user)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
     fill_in 'Password', with: 'password123'
-    fill_in 'Password confirmation', with: 'password123'
-    click_button 'Sign up'
-    expect(page).to have_text('Welcome! You have signed up successfully.')
+    click_button 'Log in'
+    sleep(1)
   end
-  # it 'allows user to login' do
-  #   user = FactoryBot.create(:user)
-  #   visit new_user_session_path
-  #   fill_in 'Email', with: user.email
-  #   fill_in 'Password', with: 'password123'
-  #   click_button 'Log in'
-  #   expect(page).to have_text('Most recent')
-  # end
+
+  it 'allows a user to create a category' do
+    visit new_group_path
+    fill_in 'Name', with: 'Test Group'
+    click_button 'Create Group'
+    select 'Rent', from: 'Icon'
+    within('form') do
+      click_button 'Create Group'
+    end
+    sleep(1)
+    expect(page).to have_content('Test Group')
+  end
+
+  it 'allows a user to see Most recent' do
+    visit root_path
+    sleep(1)
+    expect(page).to have_content('Most recent')
+  end
+
+  it 'allows a user to see Most ancient' do
+    visit root_path
+    sleep(1)
+    expect(page).to have_content('Most ancient')
+  end
+
+  it 'allows a user to see Gift credit' do
+    visit root_path
+    sleep(1)
+    expect(page).to have_content('Gift credit')
+  end
 end
